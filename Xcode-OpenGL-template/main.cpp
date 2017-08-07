f5a63168964ab7e67bb8a2fdf9e49150430af280
vinayak <vinayakdeepu@gmail.com>
2017-08-07 12:46:29 +0530

    Initial Commit

---
+++ Xcode-OpenGL-template/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode-OpenGL-template
+//
+//  Created by vinayak vivek on 8/7/17.
+//  Copyright © 2017 vinayak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
